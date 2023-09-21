c5f06050d5099374bc4c2cd09d0e53f140c0a692
bybullantus <bullantus@gmail.com>
2023-09-21 23:54:22 +0300

    update

--- farseer/src/main/java/com/bullantus/farseeer/config/OpenApiConfig.java
+++ farseer/src/main/java/com/bullantus/farseeer/config/OpenApiConfig.java
@@ -13,27 +13,19 @@ import io.swagger.v3.oas.annotations.servers.Server;
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
-                        name = "Alibou",
-                        email = "contact@aliboucoding.com",
-                        url = "https://aliboucoding.com/course"
+                        name = "Bullantus",
+                        email = "bullantus@gmail.com"
+                       
                 ),
-                description = "OpenApi documentation for Spring Security",
-                title = "OpenApi specification - Alibou",
+                description = "OpenApi documentation for Farseer",
+                title = "Farseer Api",
                 version = "1.0",
-                license = @License(
-                        name = "Licence name",
-                        url = "https://some-url.com"
-                ),
                 termsOfService = "Terms of service"
         ),
         servers = {
                 @Server(
                         description = "Local ENV",
                         url = "http://localhost:8080"
-                ),
-                @Server(
-                        description = "PROD ENV",
-                        url = "https://aliboucoding.com/course"
                 )
         },
         security = {
